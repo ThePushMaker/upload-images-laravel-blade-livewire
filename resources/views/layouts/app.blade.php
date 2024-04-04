243e120c358c90a82f916f947a6d5ab9b616781e
Martin Calderón <elmcwl@gmail.com>
2024-04-04 02:28:26 -0700

    build: installing tailwindcss with vite

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Devstagram - @yield('titulo')</title>
-</head>
-<body>
-  
-  
-  
-</body>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Devstagram - @yield('titulo')</title>
+    @vite('resources/css/app.css')
+    
+  </head>
+  <body>
+    
+    <h1 class="text-4xl font-extrabold">@yield('titulo')</h1>
+    
+  </body>
 </html>
