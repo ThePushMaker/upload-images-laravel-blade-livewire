d735c2f24e6336b252fc9e1a95fe3d5c2bcfeec1
Martin Calderón <elmcwl@gmail.com>
2024-04-17 10:13:43 -0700

    fix: login redirect error

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -23,6 +23,6 @@ class LoginController extends Controller
           return back()->with('mensaje', 'Credenciales Incorrectas');
         }
         
-        return redirect()->route('posts.index');
+        return redirect()->route('posts.index', ['user' => auth()->user()->username]);
     }
 }
