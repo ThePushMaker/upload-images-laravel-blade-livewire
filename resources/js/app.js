1f670df996eb1197f8bae3ccc5a022aeeb3cf34c
Martin Calderón <elmcwl@gmail.com>
2024-04-19 00:32:40 -0700

    feat: add dropzone events

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,3 +10,19 @@ const dropzone = new Dropzone('#dropzone', {
   maxFiles: 1,
   updloadMultiple: false, 
 });
+
+dropzone.on('sending', function(file, xhr, formData){
+  console.log(file)
+})
+
+dropzone.on('success', function (file, response) {
+  console.log(response)
+})
+
+dropzone.on('error', function (file, message) {
+  console.log(message)
+})
+
+dropzone.on('removedfile', function () {
+  console.log('Archivo eliminado')
+})
