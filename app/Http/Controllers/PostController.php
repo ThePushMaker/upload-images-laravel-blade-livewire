1586b758f43db4d8138e26f1258d3466cf60cc2d
Martin Calderón <elmcwl@gmail.com>
2024-05-11 22:30:47 -0700

    feat: add saving uploaded images in the DB

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -26,5 +27,15 @@ class PostController extends Controller
             'descripcion' => 'required',
             'imagen' => 'required'
         ]);
+        
+        Post::create([
+            'titulo' => $request->titulo,
+            'descripcion' => $request->descripcion,
+            'imagen' => $request->imagen,
+            'user_id' => auth()->user()->id
+        ]);
+        
+        // redireccionar
+        return redirect()->route('posts.index', auth()->user()->username);
     }
 }
