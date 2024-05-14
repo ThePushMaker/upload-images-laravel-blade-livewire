a2004969c3bc76c90e0f9b43b8aca702ff4cf2ad
Martin Calderón <elmcwl@gmail.com>
2024-05-14 16:31:32 -0700

    feat: add filter to posts of user

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,8 +10,11 @@ class PostController extends Controller
 {    
     public function index(User $user)
     {
+        $posts = Post::where('user_id', $user->id)->get();
+                
         return view('dashboard', [
-            'user'=> $user
+            'user'=> $user,
+            'posts' => $posts
         ]);
     }
     
