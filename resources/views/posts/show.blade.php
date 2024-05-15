ecf2de96e8d41324f39e96494c71e451a3a8efe1
Martin Calderón <elmcwl@gmail.com>
2024-05-15 01:05:18 -0700

    feat: add comments box

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -27,8 +27,40 @@
       </div>
     </div>
     
-    <div class="md:w-1/2">
-      2
+    <div class="md:w-1/2 p-5">
+      <div class="shadow bg-white p-5 mb-5">
+        <p class="text-xl font-bold text-center mb-4">Comentarios</p>
+      
+      <form>
+        <div class="mb-5">
+          <label for="comentario" class="mb-2 block uppercase text-gray-500 font-bold">
+            Añade un Comentario
+          </label>
+          <textarea
+            id="comentario"
+            name="comentario"
+            placeholder="Agrega un Comentario"
+            class="border p-3 w-full rounded-lg
+              @error('comentario')
+                border-red-500 
+              @enderror"
+          ></textarea>
+          
+          @error('comentario')
+            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+              {{ $message }}
+            </p>
+          @enderror
+        </div>
+        
+        <input
+          type="submit"
+          value="Comentar"
+          class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+        />
+      </form>
+      
+      </div>
     </div>
   </div>
 @endsection 
