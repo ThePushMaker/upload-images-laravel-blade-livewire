fb7eba5b44cfa76735f612eafb05650635878dc5
Martin Calderón <elmcwl@gmail.com>
2024-05-14 20:56:13 -0700

    feat: add display author and date of post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,4 +5,30 @@
 @endsection
 
 @section('contenido')
+  <div class="container mx-auto flex">
+    <div class="md:w-1/2">
+      <img 
+        src="{{ asset('uploads') . '/' . $post->imagen }}" 
+        alt="Imagen del post{{ $post->titulo }}"
+      />
+      
+      <div class="p-3">
+        <p>0 Likes</p>
+      </div>
+      
+      <div>
+        <p class="font-bold">{{ $post->user->username }}</p>
+        <p class="text-sm text-gray-500">
+          {{ $post->created_at->diffForHumans() }}
+        </p>
+        <p class="mt-5">
+          {{ $post->descripcion }}
+        </p>
+      </div>
+    </div>
+    
+    <div class="md:w-1/2">
+      2
+    </div>
+  </div>
 @endsection 
