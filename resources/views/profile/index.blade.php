068f23f90830597e66f00acb9690f2a0fd6ef8f1
Martin Calderón <elmcwl@gmail.com>
2024-05-17 18:54:26 -0700

    feat: add form to edit profile

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -5,4 +5,51 @@
 @endsection
 
 @section('contenido') 
+  <div class="md:flex md:justify-center">
+    <div class="md:w-1/2 bg-white shadow p-6">
+      <form
+        class="mt-10 md:mt-0"
+        action=""
+      >
+        <div class="mb-5">
+          <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+            Username
+          </label>
+          <input
+            id="username"
+            name="username"
+            type="text"
+            placeholder="Tu Nombre de Usuario"
+            class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
+            value="{{ auth()->user()->username }}"
+          />
+          @error('username')
+            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+              {{ $message }}
+            </p>
+          @enderror
+        </div>
+        
+        <div class="mb-5">
+          <label for="imagen" class="mb-2 block uppercase text-gray-500 font-bold">
+            Imagen Perfil
+          </label>
+          <input
+            id="imagen"
+            name="imagen"
+            type="file"
+            class="border p-3 w-full rounded-lg"
+            value=""
+            accept=".jpg, .jpeg, .png"
+          />
+        </div>
+      
+        <input
+          type="submit"
+          value="Guardar Cambios"
+          class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+        />
+      </form>
+    </div>
+  </div>
 @endsection
