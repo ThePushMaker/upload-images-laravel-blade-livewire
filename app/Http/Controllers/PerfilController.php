37bcae5d53c26423dccd75ab68dbfc7ceb44b91c
Martin Calderón <elmcwl@gmail.com>
2024-05-18 00:05:20 -0700

    feat: add functionality to save the modified user's data

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class PerfilController extends Controller
 {
@@ -23,5 +24,29 @@ class PerfilController extends Controller
         $validated = $request->validate([
             'username' => ['required', 'unique:users,username,'.auth()->user()->id,'min:3','max:20', 'not_in:twitter,editar-perfil'],
         ]);
-    }
+        
+        if($request->imagen)
+        {
+            $imagen = $request->file('imagen');
+        
+            // generear un nombre unico para cada imagen
+            $nombreImagen = Str::uuid() . "." . $imagen->extension();
+            
+            // la imagen se almacena en el servidor y se redimensiona
+            $imagenServidor = Image::make($imagen);
+            $imagenServidor->fit(1000, 1000);
+    
+            // creamos el path donde se guardará la imagen y se guarda allí
+            $imagenPath = public_path('profiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+        
+        // guardar cambios
+        $usuario = User::find(auth()->user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? '';
+        $usuario->save();
+        
+        // redireccionar con el usuario modificado
+        return redirect()->route('posts.index', $usuario->username);
 }
