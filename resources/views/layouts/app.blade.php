1558df2551e92c29c95d3801e3141eff04d5211f
Martin Calderón <elmcwl@gmail.com>
2024-05-20 02:42:54 -0700

    style: footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,7 +70,7 @@
     </main>
     
     <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
-      DevStagram - Todos los derechos reservados
+      TheNotInstagram - Todos los derechos reservados
       {{ now()->year }}
     </footer>
     
