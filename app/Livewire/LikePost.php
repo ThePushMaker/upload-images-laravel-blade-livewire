deea83bf37cc6b868ad2d8a6adb39785e177a8f3
Martin Calderón <elmcwl@gmail.com>
2024-05-20 00:51:53 -0700

    feat: add reactivity with livewire

--- app/Livewire/LikePost.php
+++ app/Livewire/LikePost.php
@@ -7,15 +7,23 @@ use Livewire\Component;
 class LikePost extends Component
 {
     public $post;
+    public $isLiked; //si el usuario ya le dio me gusta a este post
+    
+    public function mount($post){
+        // comprobar si el usuario ya le dio me gusta a este post
+        $this->isLiked = $post->checkLike(auth()->user());    
+    }
     
     public function like()
     {
         if( $this->post->checkLike(auth()->user() )) {
             $this->post->likes()->where('post_id', $this->post->id)->delete();
+            $this->isLiked = false;
         } else {
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id
             ]);
+            $this->isLiked = true;
         }
     }
     
