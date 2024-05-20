c249c418ee553ef1d9b736b41b225f193c601a1c
Martin Calderón <elmcwl@gmail.com>
2024-05-19 22:26:04 -0700

    feat: add events in livewire

--- app/Livewire/LikePost.php
+++ app/Livewire/LikePost.php
@@ -7,6 +7,12 @@ use Livewire\Component;
 class LikePost extends Component
 {
     public $post;
+    
+    public function like()
+    {
+        return "desde la funcion de like";
+    }
+    
     public function render()
     {
         return view('livewire.like-post');
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -18,7 +18,7 @@
         
         <livewire:like-post :post="$post" />
         
-          @if($post->checkLike(auth()->user() ))
+          {{-- @if($post->checkLike(auth()->user() ))
             <form
               method="POST"
               action="{{ route('posts.likes.destroy', $post) }}"
@@ -26,11 +26,7 @@
               @method('DELETE')
               @csrf
               <div class="my-4">
-                <button type="submit">
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
-                  </svg>
-                </button>
+                
               </div>
             </form>
           @else
@@ -47,7 +43,7 @@
                 </button>
               </div>
             </form>
-          @endif
+          @endif --}}
         @endauth
       
         <p class="font-bold">{{ $post->likes->count() }}
